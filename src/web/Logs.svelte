4f9aac5b561d6700752418f65334dcf13a7af493
pquizon <pquizon@freelancer.com>
2019-06-28 18:49:11 +0800

    Update logs view

--- src/web/Logs.svelte
+++ src/web/Logs.svelte
@@ -1,40 +1,94 @@
-<style>
-    .lds-ripple {
-        display: inline-block;
-        position: relative;
-        width: 64px;
-        height: 64px;
-    }
-    .lds-ripple div {
-        position: absolute;
-        border: 4px solid #fff;
-        opacity: 1;
-        border-radius: 50%;
-        animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
-        background-color: #B80F0A;
+<script>
+    import { LOG_FILES } from './config.json';
 
-    }
-    .lds-ripple div:nth-child(2) {
-        animation-delay: -0.5s;
-    }
-    @keyframes lds-ripple {
-        0% {
-            top: 28px;
-            left: 28px;
-            width: 0;
-            height: 0;
-            opacity: 1;
+    let currentLog = undefined;
+    let content = "";
+    let lines = [];
+
+    function handleClick(event) {
+        const LOG_FILE = event.target.id;
+
+        if (currentLog) {
+            const active = document.getElementById(currentLog);
+            active.className = active.className.replace("active", "").trim();
         }
-        100% {
-            top: -1px;
-            left: -1px;
-            width: 58px;
-            height: 58px;
-            opacity: 0;
-        }
+
+        setLog(LOG_FILE);
+    }
+
+    function setLog(logFile) {
+        currentLog = logFile;
+
+        const active = document.getElementById(currentLog);
+        active.className += " active";
+
+        // get/set content
+
+        lines = JSON.stringify(content, null, 8).split("\\n");
+    }
+
+    function getLabel(file) {
+        return file.replace(new RegExp('_', 'g'), ' ');
+    }
+</script>
+
+<style type="text/css">
+    .container {
+        display: flex;
+        width: 100vw;
+        height: 100vh;
+        font-family: 'Noto Sans HK', sans-serif;
+        font-size: 12px;
+        background-color: #272822;
+        color: #F8F8F2;
+    }
+
+    .navigation {
+        overflow-y: auto;
+    }
+
+    .navigation-item {
+        padding: 4px;
+        border: solid 1px black;
+        flex-wrap: wrap;
+        display: flex;
+        word-break: break-all;
+    }
+
+    .navigation-item.active {
+        background-color: #49483E !important;
+    }
+
+    .navigation-item:hover {
+        background-color: #3E3D32;
+    }
+
+    .content {
+        overflow-y: auto;
+        overflow-x: auto;
+        padding: 16px;
+        max-width: 70vw;
+        flex-grow: 1;
+        white-space: pre-wrap;
+        word-break: break-all;
     }
 </style>
 
-<div>
-    Lorem ipsum dolor ... Logs go here
+<div class="container">
+    <div class="navigation">
+        {#each Object.keys(LOG_FILES) as file}
+            <div
+                class="navigation-item"
+                id={file}
+                on:click={handleClick}
+            >
+                {getLabel(file)}
+            </div>
+        {/each}
+    </div>
+    <code class="content">
+        {#each lines as line}
+            {line}<br />
+        {/each}
+    </code>
 </div>
